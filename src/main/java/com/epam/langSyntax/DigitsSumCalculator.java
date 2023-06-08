ac6f043a40dc360545d7c5c683fd669ec1d67dd1
thecoocko <butenkonaddezhda@gmail.com>
2023-06-08 13:39:41 +0200

    done: langSyntax

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,14 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int sum = 0; int digit = 0;
+        while(number > 0){
+            digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
-
 }
+
+
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int rev = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            rev = rev * 10 + remainder;
+            number = number / 10;
+        }
+        System.out.println(rev);
     }
-
 }
