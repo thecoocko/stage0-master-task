fc3773a0f9368a5739f8752cf6e478dbc60979d5
thecoocko <butenkonaddezhda@gmail.com>
2023-06-08 13:24:27 +0200

    done: OOP

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,41 @@
 package com.epam.OOP;
 
+import java.text.MessageFormat;
+
+/*Work with class Animal.
+Provide it with 3 private fields - color(String), numberOfPaws(int), hasFur(boolean).
+Add constructor with full parameters. Save the parameter order as it is listed in a second paragraph.
+Add a methods getDescription(), witch would use class fields and return a string with such pattern "This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+(Optional) In the method getDescription() change the word 'paw' depending on the numberOfPaws: number of paws is 1 -> 'paw', number of paws is different from 1 -> 'paws'.*/
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color=color;
+        this.numberOfPaws=numberOfPaws;
+        this.hasFur=hasFur;
+    }
+
+    public String getDescription(){
+
+        String s = this.numberOfPaws == 1 ? "paw" : "paws";
+        String f = this.hasFur == true ? "a" : "no";
+
+        return String.format("This animal is mostly %s. It has %d %s and %s fur.",this.color,this.numberOfPaws,s,f);
+    }
+
+    protected String getColor() {
+        return color;
+    }
+
+    protected int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    protected boolean isHasFur() {
+        return hasFur;
+    }
 
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,21 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal{
+/*Override getDescription() method for class Bird: add one more sentence to the description.
+The result must be " This animal is mostly blue. It has 2 paws and no fur.
+Moreover, it has 2 wings and can fly."
+(Optional) Create an object of each class and call getDescription() method for both of them.
+Try to explain the output results.*/
+    public Bird() {
+        super("blue", 2, false);
+
+    }
+    @Override
+    public String getDescription(){
+        String s = getNumberOfPaws() == 1 ? "paw" : "paws";
+        String f = isHasFur() == true ? "a" : "no";
+        return String.format("This animal is mostly %s. It has %d %s and %s fur. Moreover, it has 2 wings and can fly.",getColor(),getNumberOfPaws(),s,f);
+
+    }
 
 }
